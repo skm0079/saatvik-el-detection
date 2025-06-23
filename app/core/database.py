6acf7f27247a9fe46e08f0116731b794ded57fd2
skm0079 <skmukherjee2021@gmail.com>
2025-06-23 12:23:52 +0530

    DB Persistance Fix

--- app/core/database.py
+++ app/core/database.py
@@ -5,24 +5,21 @@ from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 from app.core.config import settings
 
-engine = create_async_engine(
-    settings.database_url,
-    echo=True,
-    future=True
-)
+engine = create_async_engine(settings.database_url, echo=True, future=True)
 
-async_session = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        # Force drop and recreate all tables
-        await conn.run_sync(SQLModel.metadata.drop_all)
+        # Force drop and recreate all tables, if Needed Else Only Create
+        # await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
         print("✅ Database tables recreated with correct schema")
 
+
 from typing import AsyncGenerator
+
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
